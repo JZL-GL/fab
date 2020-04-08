a809ed986168b4c2d0481c32d02b4c90d65292ed
JZL <353553339@qq.com>
2020-04-08 10:29:47 +0800

    delete data by not use

--- tester/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:UTF-8 -*-
-
-from invoke import Collection, Program
-from tester import tasks
-
-program = Program(namespace=Collection.from_module(tasks), version='0.1.0')
