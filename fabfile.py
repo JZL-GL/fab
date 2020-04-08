428b6b382846ce52e1f569b8b208f067d208f9cf
JZL <353553339@qq.com>
2020-04-08 14:53:46 +0800

    modify

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,7 @@
 # -*- coding:UTF-8 -*-
 
 from fabric import Connection
-from invoke import Config
-from fabric import Config
-from fabric import Connection
-from fabric import task
-from invoke import Exit
 
-Connection.from_v1
 # from fabric import task
 #
 # from fabric import SerialGroup as Group
@@ -24,25 +18,24 @@ web02 = Connection(host="10.0.0.13", user="root", connect_kwargs={'password': "1
 # # for host in (web01, web02):
 # # result = Connection(host,user='root',connect_kwargs={'password': "123456"}).run('hostname')
 #
-# # 继承了Connection的用法
+# # 继承了Connection的用法 connect_kwargs={"key_filename": "/Users/jzl/.ssh/id_rsa"}
 # results = Group(web01,web02,user="root", connect_kwargs={'password': "123456"}).run('hostname')
 
 
 from invoke import task
 from fabric import task
 
-config = Config(overrides={'run': {'pty': 'True', 'warn': 'False'}})
+# config = Config(overrides={'run': {'pty': 'True', 'warn': 'False'}})
 
-web01 = Connection(host="10.0.0.200", user="root", connect_kwargs={"key_filename": "/Users/jzl/.ssh/id_rsa"},
-                   config=config)
+web01 = Connection(host="10.0.0.200", user="root", connect_kwargs={'password': "123456"})
 
 
 @task
 def upload_and_unpack(c):
+    # c.cd("/User")
+    # c.run('ls')
     c.run('hostname')
     web01.run('hostname -I')
     web01.run('echo $PATH;w')
     web01.run('echo $SHELL')
 
-# for host in (web01, web02):
-#     result = Connection(host).run('uname -s')
